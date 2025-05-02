737d7d4f5bdd52d2e359f35e6afe21066a7cb4dc
Roc Lai <laipeng668@qq.com>
2025-05-02 20:37:32 +0800

    精简脚本

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -6,8 +6,6 @@ sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ Build by Roc')/g" feeds/luci/modu
 # 移除要替换的包
 rm -rf feeds/packages/net/alist
 rm -rf feeds/luci/applications/luci-app-alist
-rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
 rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
@@ -20,13 +18,12 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Alist & AdGuardHome & WolPlus & AriaNg & 集客无线AC控制器 & Lucky & 雅典娜LED控制
+# AList & AdGuardHome & WolPlus & Lucky & 集客无线AC控制器 & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-git_sparse_clone main https://github.com/kenzok8/small-package adguardhome luci-app-adguardhome
+git_sparse_clone master https://github.com/kenzok8/openwrt-packages luci-app-adguardhome
 git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
-git_sparse_clone master https://github.com/immortalwrt/packages net/ariang
-git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
+git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
 git clone --depth=1 https://github.com/NONGFAH/luci-app-athena-led package/luci-app-athena-led
 chmod +x package/luci-app-athena-led/root/etc/init.d/athena_led package/luci-app-athena-led/root/usr/sbin/athena-led
 
