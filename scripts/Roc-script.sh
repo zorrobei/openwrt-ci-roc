5cc48af9eb46f236b7a54f9e811694b5560f1d6a
Roc Lai <laipeng668@qq.com>
2025-06-27 22:21:29 +0800

    精简脚本

--- scripts/Roc-script.sh
+++ scripts/Roc-script.sh
@@ -10,9 +10,6 @@ cp -f $GITHUB_WORKSPACE/patches/fix_vlmcsd_compile_with_ccache.patch feeds/packa
 # 移除要替换的包
 rm -rf feeds/packages/net/open-app-filter
 rm -rf feeds/packages/net/adguardhome
-rm -rf feeds/packages/net/ariang
-rm -rf feeds/packages/lang/golang
-rm -rf package/emortal/luci-app-athena-led
 
 # Git稀疏克隆，只克隆指定目录到本地
 function git_sparse_clone() {
@@ -24,11 +21,10 @@ function git_sparse_clone() {
   cd .. && rm -rf $repodir
 }
 
-# Go & OpenList & AdGuardHome & AriaNg & WolPlus & Lucky & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
+# OpenList & AdGuardHome & WolPlus & Lucky & OpenAppFilter & 集客无线AC控制器 & 雅典娜LED控制
 git clone --depth=1 https://github.com/sbwml/luci-app-openlist package/openlist
 git_sparse_clone master https://github.com/kenzok8/openwrt-packages adguardhome luci-app-adguardhome
-git_sparse_clone master https://github.com/laipeng668/packages ariang luci-app-wolplus
+git_sparse_clone main https://github.com/VIKINGYFY/packages luci-app-wolplus
 git clone --depth=1 https://github.com/gdy666/luci-app-lucky package/luci-app-lucky
 git clone --depth=1 https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
 git clone --depth=1 https://github.com/lwb1978/openwrt-gecoosac package/openwrt-gecoosac
